63286114519057c2cb33e90969a92b73339289e9
tjdqls1200 <dltjdqls1200@naver.com>
2022-12-08 14:00:14 +0900

    Time: 1 ms (77.58%), Space: 41.8 MB (52.43%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -20,18 +20,8 @@ class Solution {
         
         DFS(root1, root1Leaf);
         DFS(root2, root2Leaf);
-        
-        if (root1Leaf.size() != root2Leaf.size()) {
-            return false;
-        }
-        
-        for (int i = 0; i < root1Leaf.size(); i++) {
-            if (root1Leaf.get(i) != root2Leaf.get(i)) {
-                return false;
-            }
-        }
-        
-        return true;
+
+        return root1Leaf.equals(root2Leaf);
     }
     
     public void DFS(TreeNode root, List<Integer> rootLeaf) {
