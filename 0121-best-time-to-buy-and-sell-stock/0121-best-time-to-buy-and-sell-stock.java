c6a6f0075fd73c3a443eba8f7c66d402e602b847
tjdqls1200 <dltjdqls1200@naver.com>
2022-12-13 00:23:14 +0900

    Time: 1 ms (100.00%), Space: 59.1 MB (87.94%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,15 +3,11 @@ class Solution {
         int min = Integer.MAX_VALUE;
         int profit = 0;
         
-        if (prices.length == 1) {
-            return 0;
-        }
-        
         for (int price : prices) {
             if (price < min) {
                 min = price;
-                continue;
             }
+            
             profit = Math.max(profit, price - min);
         }
         
