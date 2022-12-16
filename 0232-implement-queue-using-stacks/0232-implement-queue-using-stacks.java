6e9e1ae059b6246a0d1c0e565c2bf4fa08a67166
tjdqls1200 <dltjdqls1200@naver.com>
2022-12-16 11:40:52 +0900

    Time: 0 ms (100.00%), Space: 39.9 MB (96.30%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -2,11 +2,6 @@ class MyQueue {
     Stack<Integer> in = new Stack<>();
     Stack<Integer> out = new Stack<>();
     
-    public MyQueue() {
-        // 3 2 1
-        // 1 2 3
-    }
-    
     public void push(int x) {
         in.push(x);
     }
@@ -19,11 +14,9 @@ class MyQueue {
         return popNum;
     }
     
-    private void change(Stack in, Stack out) {
-        int size =  in.size();
-        
-        for (int i = 0; i < size; i++) {
-            out.push(in.pop());
+    private void change(Stack a, Stack b) {
+        while (!a.isEmpty()) {
+            b.push(a.pop());
         }
     }
     
